af03608c0ecaf285a33a3f87e6d94a9839caaed5
aleksandarj93 <aleksandar.jankovic@saga.rs>
2018-06-06 14:54:43 +0200

    komit na masteru

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var cities = {cities:["Amsterdam","Berlin","New York","San Francisco","Tokyo", "Beograd", "Lucani", "Kraljevo"]}
 var nesto = "Ovo je samo test da da!"
-var igrnore = "ignore this branch"
+var bla = "ovde se izvrsava wercker"
 
 app.get('/', function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
